c07360b0e58a793dd6484c992d6c20e588f6a496
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-15 21:47:15 +0100

    add transactions

--- queries.sql
+++ queries.sql
@@ -16,6 +16,38 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+/* Transaction 1*/
+BEGIN;
+
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+
+COMMIT;
+
+/* Transactio 2*/
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+/* Transactio 3*/
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP1;
+
+UPDATE animals SET weight_kg = (weight_kg * -1);
+
+ROLLBACK TO SP1;
+
+UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
+
+COMMIT;
+
+/* Trasaction 4*/
 SELECT COUNT(*) AS "NUMBER OF ANIMALS" FROM animals;
 
 SELECT COUNT(*) AS "NUMBER OF ANIMALS THAT HAVE NEVER TRIED TO ESCAPE" FROM animals WHERE escape_attempts = 0;
