a5aa44cec88d55cf8d5fe0428838f22545b63e26
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-15 20:37:34 +0100

    add query aggregate functions

--- queries.sql
+++ queries.sql
@@ -15,3 +15,15 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+SELECT COUNT(*) AS "NUMBER OF ANIMALS" FROM animals;
+
+SELECT COUNT(*) AS "NUMBER OF ANIMALS THAT HAVE NEVER TRIED TO ESCAPE" FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) AS "AVERAGE WEIGHT OF ANIMALS" FROM animals;
+
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
