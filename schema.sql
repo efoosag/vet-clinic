2e5255921a0a28873ab1046456226e2d7dd8daba
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-16 16:00:19 +0100

    add JOIN TABLES

--- schema.sql
+++ schema.sql
@@ -28,3 +28,16 @@ SETVAL('animals_id_seq', (select max(id) from animals), false);
  ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
  ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+ -- Join Table
+
+ CREATE TABLE vets (id SERIAL PRIMARY KEY, name VARCHAR(100), age INT, date_of_graduation DATE);
+
+ CREATE TABLE specializations (species_id INT, vets_id INT, 
+ CONSTRAINT fk_sp FOREIGN KEY(species_id) REFERENCES species(id), 
+ CONSTRAINT fk_ve FOREIGN KEY(vets_id) REFERENCES vets(id));
+
+ CREATE TABLE visits (animals_id INT, vets_id INT, date_of_visit DATE, 
+ CONSTRAINT fk_an FOREIGN KEY(animals_id) REFERENCES animals(id), 
+ CONSTRAINT fk_ve FOREIGN KEY(vets_id) REFERENCES vets(id));
+
