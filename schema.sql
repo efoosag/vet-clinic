ad6b3d63509aa6d279c76905a92e42b6a987a923
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-16 10:16:20 +0100

    add owners and species tables

--- schema.sql
+++ schema.sql
@@ -9,3 +9,22 @@ CREATE TABLE ANIMALS (
     WEIGHT_KG DECIMAL);
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+-- Multiple Tables
+
+CREATE TABLE owners (id SERIAL PRIMARY KEY, full_name VARCHAR(100), age INT);
+
+CREATE TABLE species (id SERIAL PRIMARY KEY, name VARCHAR(100));
+
+--Modify id column of animals table(autoincrement and primary key)
+CREATE SEQUENCE animals_id_seq OWNED BY animals.id;
+
+SETVAL('animals_id_seq', (select max(id) from animals), false);
+
+ ALTER TABLE animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
+
+ ALTER TABLE animals DROP COLUMN species;
+
+ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+
+ ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
